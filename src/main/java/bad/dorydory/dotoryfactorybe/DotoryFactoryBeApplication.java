7f9f8b80d761d7e5f99ec2de5b55bf1c36c0f4bb
이예린 <ebbunbul@gmail.com>
2023-02-16 21:49:46 +0900

    init

---
+++ src/main/java/bad/dorydory/dotoryfactorybe/DotoryFactoryBeApplication.java
@@ -0,0 +1,13 @@
+package bad.dorydory.dotoryfactorybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoryFactoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoryFactoryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bad/dorydory/dotoryfactorybe/DotoryFactoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package bad.dorydory.dotoryfactorybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoryFactoryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
